63d60f03d43ae666332e7a342b48847caa33cda0
thyoop <so_re@naver.com>
2025-02-19 10:58:26 +0900

    장바구니와 주문페이지 단순 퍼블리싱

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Cart from "./components/Cart";
+import Order from "./components/Order";
+
+function App() {
+  return (
+    <>
+      <Order></Order>
+      {/* <Cart /> */}
+    </>
+  );
+}
+
+export default App;
