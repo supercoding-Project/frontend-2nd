55d623d1a218a6b1507c35215cc84c9b73d297fd
mongchongi <yeonguk0517@gmail.com>
2025-02-21 14:51:31 +0900

    도서 리스트 메인 페이지에 렌더링

--- shoppingMall/src/pages/Home/Home.jsx
+++ shoppingMall/src/pages/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import MainBanner from "../../components/Home/MainBanner";
 import BookCategories from "../../components/Home/BookCategories";
 import PopularBooks from "../../components/Home/PopularBooks";
+import BookController from "../../components/book/BookController";
 
 const Home = () => {
   return (
     <div>
       <MainBanner />
-      <BookCategories />
-      <PopularBooks />
+      {/* <BookCategories /> */}
+      {/* <PopularBooks /> */}
+      <BookController />
     </div>
   );
 };
