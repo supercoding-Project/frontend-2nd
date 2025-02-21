cf597a3b47fce7bb8ba7eb25375e0b66b3658fe5
mongchongi <yeonguk0517@gmail.com>
2025-02-21 17:39:40 +0900

    회원가입 페이지 추가

--- shoppingMall/src/App.jsx
+++ shoppingMall/src/App.jsx
@@ -4,6 +4,7 @@ import Layout from "./components/layout/Layout";
 import Home from "./pages/Home/Home";
 import BookList from "./pages/BookList";
 import Login from "./pages/login/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/books" element={<BookList />} />
         <Route path="/logIn" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
     </Layout>
   );
