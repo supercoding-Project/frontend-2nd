7ddf1965c158d32512c9ed8280ee153727ac509a
mongchongi <yeonguk0517@gmail.com>
2025-02-25 20:23:21 +0900

    도서 리스트 및 카테고리 컴포넌트 삭제

--- shoppingMall/src/pages/Home/Home.jsx
+++ shoppingMall/src/pages/Home/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import MainBanner from "../../components/Home/MainBanner";
 import BookCategories from "../../components/Home/BookCategories";
 import PopularBooks from "../../components/Home/PopularBooks";
-import BookController from "../../components/book/BookController";
 
 const Home = () => {
   return (
@@ -10,7 +9,6 @@ const Home = () => {
       <MainBanner />
       {/* <BookCategories /> */}
       {/* <PopularBooks /> */}
-      <BookController />
     </div>
   );
 };
