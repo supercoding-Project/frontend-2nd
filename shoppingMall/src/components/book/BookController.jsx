c2e1e1228faf79f663322edaf9453c79a596888b
mongchongi <yeonguk0517@gmail.com>
2025-02-26 17:13:48 +0900

    도서 리스트 UI 구현

---
+++ shoppingMall/src/components/book/BookController.jsx
@@ -0,0 +1,83 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import BookList from "./BookList";
+import { AiOutlineLoading } from "react-icons/ai";
+import styles from "./BookController.module.css";
+
+const BookController = () => {
+  const [books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const badgeColor = {
+    최상: "#3471F0",
+    상: "#6293E7",
+    중: "#738DB5",
+    하: "#6D7480",
+  };
+
+  const NewlyAddedBooks = books.slice(books.length - 6);
+
+  const startIndex = (currentPage - 1) * 18;
+  const endIndex = startIndex + 18;
+  const currentBooks = books.slice(startIndex, endIndex);
+
+  const fetchBooks = async () => {
+    setIsLoading(true);
+
+    try {
+      const res = await axios.get("http://localhost:5981/display/all");
+      setBooks(res.data.content);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const totalPage = Math.ceil(books.length / 18);
+
+  const handlePagination = (selectedButton) => {
+    if (selectedButton === "prev" && currentPage !== 1) {
+      setCurrentPage((prevCurrentPage) => prevCurrentPage - 1);
+    } else if (selectedButton === "next" && currentPage !== totalPage) {
+      setCurrentPage((prevCurrentPage) => prevCurrentPage + 1);
+    }
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, 987);
+  }, [currentPage]);
+
+  if (isLoading) {
+    return (
+      <div className={`${styles["container"]} ${styles["fz-80"]}`}>
+        <AiOutlineLoading />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div className={styles["container"]}>{error.message}</div>;
+  }
+
+  return (
+    <>
+      <BookList title={"새로 등록된 도서"} books={NewlyAddedBooks} badgeColor={badgeColor} />
+      <BookList
+        title={"전체 도서"}
+        books={currentBooks}
+        badgeColor={badgeColor}
+        currentPage={currentPage}
+        handlePagination={handlePagination}
+      />
+    </>
+  );
+};
+
+export default BookController;
--- shoppingMall/src/pages/Home/Home.jsx
+++ shoppingMall/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import MainBanner from "../../components/Home/MainBanner";
 import BookCategories from "../../components/Home/BookCategories";
 import PopularBooks from "../../components/Home/PopularBooks";
+import BookController from "../../components/book/BookController";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <MainBanner />
       {/* <BookCategories /> */}
       {/* <PopularBooks /> */}
+      <BookController />
     </div>
   );
 };
