0b27cd44dfee78694a195cf4535f744b640376be
mongchongi <yeonguk0517@gmail.com>
2025-02-28 12:14:38 +0900

    Merge branch 'dev' of https://github.com/supercoding-Project/frontend-2nd into feat/product-detail

--- shoppingMall/src/App.jsx
+++ shoppingMall/src/App.jsx
@@ -1,25 +1,28 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Layout from "./components/layout/Layout";
-import Home from "./pages/Home/Home";
-import BookList from "./pages/BookList";
-import Login from "./pages/login/Login";
-import MyPage from "./pages/MyPage/MyPage";
-import Order from "./pages/Order/Order";
-import AddProduct from "./pages/AddProduct/AddProduct";
-import Detail from "./pages/detail/Detail";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import Home from './pages/Home/Home';
+import BookList from './pages/BookList';
+import Login from './pages/login/Login';
+import MyPage from './pages/MyPage/MyPage';
+import Order from './pages/Order/Order';
+import AddProduct from './pages/AddProduct/AddProduct';
+import SocialLoginCallback from './components/login/SocialLoginCallback';
+import Detail from './pages/detail/Detail';
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/books" element={<BookList />} />
-        <Route path="/logIn" element={<Login />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/order" element={<Order />} />
-        <Route path="/addproduct" element={<AddProduct />} />
-        <Route path="/detail/:bookId" element={<Detail />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/books' element={<BookList />} />
+        <Route path='/logIn' element={<Login />} />
+        <Route path='/oauth2/code/google' element={<SocialLoginCallback />} />
+        <Route path='/oauth2/code/kakao' element={<SocialLoginCallback />} />
+        <Route path='/mypage' element={<MyPage />} />
+        <Route path='/order' element={<Order />} />
+        <Route path='/addproduct' element={<AddProduct />} />
+        <Route path='/detail/:bookId' element={<Detail />} />
       </Routes>
     </Layout>
   );
